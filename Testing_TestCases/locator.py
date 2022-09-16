668aeb5a84a929c72412e1d16179c746425d92bc
Mohit <76819254+mohit11R@users.noreply.github.com>
2022-09-16 08:51:36 +0530

    Delete Testing_TestCases directory

--- Testing_TestCases/locator.py
+++
@@ -1,8 +0,0 @@
-from selenium.webdriver.common.by import By
-
-
-class MainPageLocators(object):
-    
-    WatchVideosButton = (By.XPATH , "/html/body/div[5]/div[2]/header/nav[1]/ul/li[7]/a/span/span[1]" )
-    
-    
